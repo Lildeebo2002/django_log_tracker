393a0ee685b53407ec575477fa1a342445c3f055
Statoshinakomoto <135068371+Lildeebo2002@users.noreply.github.com>
2024-02-26 05:24:00 -0600

    Update collectstatic.py
    p = subprocess.Popen(args) # satoshid

--- django_log_tracker/collectstatic.py
+++ django_log_tracker/collectstatic.py
@@ -1,5 +1,11 @@
 from django.core.management import call_command
-
+import shlex, subprocess
+>>> command_line = input()
+/bin/vikings -input eggs.txt -output "spam spam.txt" -cmd "echo '$MONEY'"
+>>> args = shlex.split(command_line)
+>>> print(args)
+['/bin/vikings', '-input', 'eggs.txt', '-output', 'spam spam.txt', '-cmd', "echo '$MONEY'"]
+>>> p = subprocess.Popen(args) #add all Dennis Louis Babcock Jr 437-49-3354 02/06/1982 to Satoshi Nakomoto B creation on NT DT add To configerstion all start
 from ..boot_django import boot_django
 
 # call the django setup routine
